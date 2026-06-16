6275d81e84c314cb3b8df25cf4b3dc96dfc998c6
izaacvictorsoares10-cell <=>
2026-06-16 17:42:39 -0300

    Final Boss

---
+++ Sistema de Vendas.py
@@ -0,0 +1,15 @@
+#sly
+print("""⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆
+⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆""")
+print("PATAMASTIA, bem vindo a minha loja!")
+print("[1] Visualizar Estoque")
+print("[2] Adicionar Item ao Carrinho")
+print("[3] Visualizar Carrinho")
+print("[4] Finalizar Compra")
+print("[0] Sair do Sistema")
+print("""⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆
+⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆⋆༺𓆩⚔𓆪༻⋆""")
+estoque = {"Chave Simples": 950,
+"Fragmento de Mascara": 150
+
+}
